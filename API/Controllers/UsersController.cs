c3fffb9b739b3202b28d98ac2a6f052639f63b61
thinga <32902065+thinga@users.noreply.github.com>
2021-03-25 16:02:37 +0100

    User,Service,Token,Dto,Controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,8 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,20 +17,21 @@ namespace API.Controllers
         public UsersController(DataContext context)
         {
             _context = context;
-
         }
+
         [HttpGet]
+        [AllowAnonymous]
+
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-             return await _context.Users.ToListAsync();
-           
+            return await _context.Users.ToListAsync();
         }
-
+        [Authorize]
         [HttpGet("{id}")]
+
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-           return await _context.Users.FindAsync(id);
+            return await _context.Users.FindAsync(id);
         }
-
     }
 }
